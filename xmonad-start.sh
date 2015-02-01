d6aa4bf701c050825ce54489c2de9c94788da1ee
Patrick Rose <pjr0911025@googlemail.com>
2015-02-01 14:27:01 +0000

    Add emacs daemon

--- xmonad-start.sh
+++ xmonad-start.sh
@@ -11,13 +11,19 @@ setxkbmap -layout gb
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
 
 xscreensaver -no-splash &
- 
+
 if [ -x /usr/bin/nm-applet ] ; then
-   nm-applet --sm-disable &
+    nm-applet --sm-disable &
 fi
 
 if [ -x /usr/bin/pasystray ] ; then
-   pasystray &
+    pasystray &
 fi
- 
-(sleep 2 && dropboxd) &
+
+if [ -x /usr/bin/dropboxd ] ; then
+    (sleep 2 && dropboxd) &
+fi
+
+if [ -x /usr/bin/emacs ] ; then
+    emacs --daemon
+fi
